1334d0af07f1a331ad79cfa1824bf6922719134f
Pruebas889 <pruebaslogin360@gmail.com>
2025-07-18 08:21:23 -0500

    Add entrypoint.sh for DocusealCopservir project

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,10 @@ cd /app
 echo "⏳ Instalando gems si hacen falta..."
 bundle check || bundle install --jobs 4 --retry 3
 
-# echo "📦 Instalando paquetes JavaScript..."
-# yarn install --check-files --network-timeout 600000
+echo "📦 Instalando paquetes JavaScript..."
+yarn install --check-files
 
-# echo "⚙️ Compilando assets..."
-# bin/shakapacker
-
-echo "Starting Rails server..."
-exec bundle exec rails server -b 0.0.0.0 -p 3000
+echo "⚙️ Compilando assets..."
+bin/shakapacker
 
 exec "$@"
